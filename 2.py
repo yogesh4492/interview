04028a5135158f6895732df9dbd5025238fe2b12
yogesh4492 <you@example.com>
2025-09-04 17:35:11 +0530

    modified:   2.py 	new file:   practice.ipynb

--- 2.py
+++ 2.py
@@ -19,11 +19,34 @@
 # print(min)
 
 
-list=[1,2,3,5,-5,-6]
-list2=[i for i in list if i<0]
-print(list2)
-count=0
-print(['negative' if i<0  else 'positive' for i in list ])
+# list=[1,2,3,5,-5,-6]
+# list2=[i for i in list if i<0]
+# print(list2)
+# count=0
+# print(['negative' if i<0  else 'positive' for i in list ])
 
-count1=[count:=count+1 for i in list if i<0]
-print(count1[-1])
+# count1=[count:=count+1 for i in list if i<0]
+# print(len(count1))
+# from functools import reduce
+
+# def add(num1,num2):
+#     return num1+num2
+# list=[1,2,34,5,6]
+# ans=reduce(add,list)
+# print(ans)
+
+lst=[1,234,56,56,76,98]
+def add(num1):
+    return num1+10
+
+lst_sum=tuple(map(add,lst))
+print(lst_sum)
+
+
+# lst=[1,2,3,4,-5]
+
+# def find(num1):
+#     if num1<0:
+#        return num1
+# lst2=list(filter(find,lst))
+# print(lst2)
